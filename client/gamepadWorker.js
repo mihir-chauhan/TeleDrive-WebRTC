545cbd508cdae05a20bec248c7cdb58710ec9d44
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2021-02-03 00:14:23 -0800

    Windows is not defined error when accessing in another js file

--- client/gamepadWorker.js
+++ client/gamepadWorker.js
@@ -10,6 +10,13 @@ onmesage = function (e) {
     }
 }
 
+window.addEventListener("gamepadconnected", function (e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index, e.gamepad.id,
+        e.gamepad.buttons.length, e.gamepad.axes.length);
+});
+
+
 while (true) {
     postMessage("HI");
 }
