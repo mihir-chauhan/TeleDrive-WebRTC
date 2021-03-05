ca34dfefc08f358ac7564166e114e095ff24a9cd
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2021-03-04 23:23:35 -0800

    Updated Server to Match Full-Stack App

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const HTTP_PORT = 8001; //default port for http is 80
 
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
+// const https = require('https');
 const WebSocket = require('ws');
 var dgram = require("dgram");
 
@@ -11,10 +11,10 @@ var dgram = require("dgram");
 const WebSocketServer = WebSocket.Server;
 
 // Yes, TLS is required
-const serverConfig = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
+// const serverConfig = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem'),
+// };
 
 // ----------------------------------------------------------------------------------------
 
@@ -37,7 +37,7 @@ const handleRequest = function (request, response) {
   }
 };
 
-const httpsServer = https.createServer(serverConfig, handleRequest);
+const httpsServer = http.createServer(handleRequest);
 httpsServer.listen(HTTPS_PORT);
 
 // ----------------------------------------------------------------------------------------
@@ -47,6 +47,7 @@ const wss = new WebSocketServer({ server: httpsServer });
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
+    console.log("msg: " + message);
     // Broadcast any received message to all clients
     if (message.includes("IP: ")) {
       message = message.replace('IP: ', '')
@@ -79,15 +80,15 @@ console.log('Server running successfully.'
 // ----------------------------------------------------------------------------------------
 
 // Separate server to redirect from http to https
-http.createServer(function (req, res) {
-  console.log(req.headers['host'] + req.url);
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
-}).listen(HTTP_PORT);
+// http.createServer(function (req, res) {
+//   console.log(req.headers['host'] + req.url);
+//   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//   res.end();
+// }).listen(HTTP_PORT);
 
 function ValidateIPaddress(ipaddress) {
   if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
     return (true)
   }
   return (false)
-}  
+}
